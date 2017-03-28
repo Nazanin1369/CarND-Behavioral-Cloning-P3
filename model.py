557dfd44cd69e771eb0289533f84f7158b5d8114
Renel Babia <renelbabia@MacBook-Pro.local>
2017-03-28 10:22:20 -0700

    Update project files with final solution

--- model.py
+++ model.py
@@ -18,71 +18,13 @@ from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 from keras.callbacks import ModelCheckpoint
 
+from myutils import *
+
 LEARNING_RATE = .0001
-EPOCHS = 5
-STEERING_CORRECTION = 0.28
+EPOCHS = 3
+#STEERING_CORRECTION = 0.28
 
 MODEL_FILE = 'model.h5'
-DRIVING_LOG = './data/driving_log.csv'
-
-def read_driving_log(filename):
-    raw_samples = []
-    with open(filename) as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            raw_samples.append(line)
-
-    return raw_samples[1:]
-
-# remove data closer to zero angle to improve data distribution
-def filter_straight_line(samples):
-    filtered = []
-
-    for s in samples:
-        steering_center = float(s[3])
-
-        if (abs(steering_center) < 0.01):
-            continue
-        else:
-            filtered.append(s)
-
-    print('Num samples after filtering data: ', len(filtered))  
-
-    return filtered          
-
-# Use the left and right camera images 
-# Use the center camera steering angle and add a correction factor
-def augment_with_lf_camera(samples):
-    augmented_sample = []
-
-    for s in samples:
-        steering_center = float(s[3])
-
-        augmented_sample.append([s[0], steering_center])
-
-        correction = STEERING_CORRECTION
-        steering_left = steering_center + correction
-        steering_right = steering_center - correction
-
-        augmented_sample.append([s[1], steering_left])
-        augmented_sample.append([s[2], steering_right])
-
-    print('Num samples after adding left/right camera: ', len(augmented_sample)) 
-
-    return augmented_sample
-
-# taken from Vivek Yadav
-def change_brightness(image):
-    # Randomly select a percent change
-    change_pct = np.random.uniform(0.4, 1.2)
-    
-    # Change to HSV to change the brightness V
-    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    hsv[:,:,2] = hsv[:,:,2] * change_pct
-    
-    #Convert back to RGB 
-    new_image = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-    return new_image
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -95,26 +37,17 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './data/IMG/'+batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
-                center_angle = batch_sample[1]
 
-                images.append(center_image)
-                angles.append(center_angle)
+                name = batch_sample.directory +  batch_sample.image_file
 
-                # add data for higher angle steering
-                if (abs(center_angle) > .30 ):
-                    curve_img = change_brightness(center_image)
-                    images.append(curve_img)
-                    angles.append(center_angle)
+                image = cv2.imread(name)
 
-                    images.append(np.fliplr(curve_img))
-                    angles.append(center_angle * -1.0)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-                # Add flipped image and steering
-                flipped = np.fliplr(center_image)
-                images.append(flipped)
-                angles.append(center_angle * -1.0)
+                angle = batch_sample.steering_angle
+
+                images.append(image)
+                angles.append(angle)
 
 
             # trim image to only see section with road
@@ -127,23 +60,29 @@ def generator(samples, batch_size=32):
 def get_model():
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-    model.add(Cropping2D(cropping=((70,25),(0,0))))
+    model.add(Cropping2D(cropping=((60,25),(0,0))))
 
-    model.add(Convolution2D(24,5,5, subsample=(2,2),activation='relu'))
-    model.add(Convolution2D(36,5,5, subsample=(2,2),activation='relu'))
-    model.add(Convolution2D(48,5,5, subsample=(2,2),activation='relu'))
+    model.add(Convolution2D(24,5,5, subsample=(2,2)))
+    model.add(ELU())
+    model.add(Convolution2D(36,5,5, subsample=(2,2)))
+    model.add(ELU())
+    model.add(Convolution2D(48,5,5, subsample=(2,2)))
+    model.add(ELU())
 
-    model.add(Convolution2D(64,3,3, activation='relu'))
-    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Convolution2D(64,3,3))
+    model.add(ELU())
+    model.add(Convolution2D(64,3,3))
+    model.add(ELU())
 
     model.add(Flatten())
-    model.add(Dropout(0.3))
-    model.add(Dense(1164))
-    model.add(Dropout(0.3))
     model.add(Dense(100))
+    model.add(Dropout(0.3))
     model.add(Dense(50))
+    model.add(Dropout(0.3))
     model.add(Dense(10))
     model.add(Dense(1))
+
+    model.summary()
 
     # Using the ADAM optimizer with a custom starting learning rate
     optimizer = Adam(lr=LEARNING_RATE)
@@ -153,22 +92,19 @@ def get_model():
     return model    
 
 
+
 def main():
 
-    t_samples, v_samples = train_test_split(read_driving_log(DRIVING_LOG), test_size=0.2)
+    samples = get_driving_data(read_driving_log(DRIVING_LOG))
 
-    print('Number of train samples     : ', len(t_samples))
-    print('Number of validation samples: ', len(v_samples))
+    train_samples, validation_samples = train_test_split(augment_and_visualize_data(samples), test_size=0.2)
 
-    train_samples = augment_with_lf_camera(filter_straight_line(t_samples))
-    validation_samples = augment_with_lf_camera(filter_straight_line(v_samples))
-
- 
-    print('train_samples[0]', train_samples[0])
+    print('Number of train samples     : ', len(train_samples))
+    print('Number of validation samples: ', len(validation_samples))
 
     # compile and train the model using the generator function
-    train_generator = generator(train_samples, batch_size=128)
-    validation_generator = generator(validation_samples, batch_size=128)
+    train_generator = generator(train_samples, batch_size=32)
+    validation_generator = generator(validation_samples, batch_size=32)
 
     model = get_model()
 
@@ -177,14 +113,16 @@ def main():
     # http://machinelearningmastery.com/check-point-deep-learning-models-keras/
     # https://keras.io/callbacks/#create-a-callback
     checkpointer = ModelCheckpoint(
-        filepath="weights.best.hdf5", verbose=1, save_best_only=True)
+        filepath=MODEL_FILE + "-epoch-{epoch:02d}", verbose=1, save_best_only=True)
 
     print("Training the model ...")
 
+    samples_per_epoch = len(train_samples)
+    #samples_per_epoch = en(train_samples) * 3
     history_object = model.fit_generator(train_generator, samples_per_epoch =
-        len(train_samples) * 3, validation_data = 
+        samples_per_epoch, validation_data = 
         validation_generator, callbacks=[early_stopping, checkpointer],
-        nb_val_samples = len(validation_samples) * 3, 
+        nb_val_samples = len(validation_samples), 
         nb_epoch=EPOCHS, verbose=1)
 
     ### print the keys contained in the history object
---
+++ myutils.py
@@ -0,0 +1,268 @@
+import os
+import csv
+import cv2
+
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+DRIVING_LOG = './data/driving_log.csv'
+STEERING_CORRECTION = 0.28
+
+orig_image_dir = './data/IMG/'
+aug_image_dir = './data/augment/'
+visualize_root = './data/visualize/'
+
+class DrivingData:
+	image_file = None
+	steering_angle = 0.0
+	directory = None
+
+	def __init__(self, image, angle, directory):
+		self.image_file = image
+		self.steering_angle = angle
+		self.directory = directory
+
+def read_driving_log(filename):
+	raw_samples = []
+	with open(filename) as csvfile:
+	    reader = csv.reader(csvfile)
+	    for line in reader:
+	        raw_samples.append(line)
+
+	return raw_samples[1:]
+
+def get_driving_data(data):
+	driving_data = []
+
+	for s in data:
+
+		name = s[0].split('/')[-1]
+		steering = float(s[3])
+
+		driving_data.append(DrivingData(name, steering, orig_image_dir))
+
+	return driving_data
+
+def filter_low_steering_angles(data, threshold=0.01, keeppct=0.10):
+	driving_data = []
+
+	for dd in data:
+		steering = dd.steering_angle
+
+		if (abs(steering) > 0.9):
+			print('anomaly: ', dd.image_file, ' steering: ', steering)
+			continue
+
+		if (abs(steering) < threshold):
+
+			# only include a 10th of the near-zero steering angles
+			if np.random.random_sample() < keeppct:
+				driving_data.append(dd)
+		else:
+			driving_data.append(dd)
+
+	return driving_data
+
+# Use the left and right camera images 
+# Use the center camera steering angle and add a correction factor
+def augment_with_lf_camera(data, threshold=0.20):
+	driving_data = []
+
+	for dd in data:
+		driving_data.append(dd)
+
+		steering = dd.steering_angle
+
+		# if greater than a threshold value, add values
+		# for left and right camera data
+		if (abs(steering) > threshold):
+			correction = STEERING_CORRECTION
+			steering_left = steering + correction
+			steering_right = steering - correction
+
+			fn = dd.image_file
+
+			left_fn = fn.replace('center', 'left')
+			driving_data.append(DrivingData(left_fn, steering_left, orig_image_dir))
+
+			right_fn = fn.replace('center', 'right')
+			driving_data.append(DrivingData(right_fn, steering_right, orig_image_dir))
+
+	return driving_data
+
+
+
+# taken from Vivek Yadav
+def change_brightness(image, angle):
+    # Randomly select a percent change
+    change_pct = np.random.uniform(0.4, 1.2)
+    
+    # Change to HSV to change the brightness V
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    hsv[:,:,2] = hsv[:,:,2] * change_pct
+    
+    #Convert back to RGB 
+    image = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
+    return image, angle
+
+# taken from Vivek Yadav
+def add_shadow(image, angle):
+    top_y = 320*np.random.uniform()
+    top_x = 0
+    bot_x = 160
+    bot_y = 320*np.random.uniform()
+    image_hls = cv2.cvtColor(image,cv2.COLOR_RGB2HLS)
+    shadow_mask = 0*image_hls[:,:,1]
+    X_m = np.mgrid[0:image.shape[0],0:image.shape[1]][0]
+    Y_m = np.mgrid[0:image.shape[0],0:image.shape[1]][1]
+
+    shadow_mask[((X_m-top_x)*(bot_y-top_y) -(bot_x - top_x)*(Y_m-top_y) >=0)]=1
+    #random_bright = .25+.7*np.random.uniform()
+    if np.random.randint(2) == 1:
+        random_bright = .5
+        cond1 = shadow_mask==1
+        cond0 = shadow_mask==0
+        if np.random.randint(2) == 1:
+            image_hls[:,:,1][cond1] = image_hls[:,:,1][cond1]*random_bright
+        else:
+            image_hls[:,:,1][cond0] = image_hls[:,:,1][cond0]*random_bright    
+    image = cv2.cvtColor(image_hls,cv2.COLOR_HLS2RGB)
+
+    return image, angle
+
+# taken from Vivek Yadav
+def shift_image(image, angle):
+    rows, cols, _ = image.shape
+    transRange = 100
+    numPixels = 10
+    valPixels = 0.4
+    transX = transRange * np.random.uniform() - transRange/2
+    angle = angle + transX/transRange * 2 * valPixels
+    transY = numPixels * np.random.uniform() - numPixels/2
+    transMat = np.float32([[1,0, transX], [0,1, transY]])
+    image = cv2.warpAffine(image, transMat, (cols, rows))
+
+    return image, angle
+
+def flip_image(image, angle):
+    image = np.fliplr(image)
+    angle = angle * -1.0
+
+    return image, angle
+
+def augment(func, data, prefix, threshold=0.20):
+	driving_data = []
+
+	fnprfx = prefix
+
+	print('augmenting ... : ', prefix)
+
+	for dd in data:
+		steering = dd.steering_angle
+
+		# if greater than a threshold value, add values
+		# for left and right camera data
+		if (abs(steering) > threshold):
+
+			src_fn = orig_image_dir + dd.image_file
+			aug_fn = aug_image_dir + fnprfx + dd.image_file
+
+			#print(src_fn)
+			image = cv2.imread(src_fn)
+
+			image, angle = func(image, steering)
+
+			#print('writing image ... : ', aug_fn)
+			cv2.imwrite(aug_fn, image)
+
+			driving_data.append(DrivingData(fnprfx + dd.image_file, angle, aug_image_dir))
+
+
+	return driving_data
+
+def plot_histogram(driving_data, bins, fn, title):
+	data = list(map(lambda d: d.steering_angle, driving_data))
+
+	n_data = len(data)
+	print ("Number of samples: ", n_data)
+
+	num_bins = bins
+	samples_per_bin = n_data/num_bins
+	hist, bins = np.histogram(data, num_bins)
+
+	fig = plt.figure()
+
+	width = 0.9 * (bins[1] - bins[0])
+	center = (bins[:-1] + bins[1:]) / 2
+	plt.bar(center, hist, align='center', width=width)
+	plt.plot((np.min(data), np.max(data)), (samples_per_bin, samples_per_bin), 'k-')
+
+	plt.title(title)
+	
+	fig.savefig(fn)
+
+# Use to visualize raw data
+def augment_and_visualize_data(data):
+	nbin = 30
+
+	current_data = []
+
+	# let's see distribution for raw data
+	plot_histogram(data, nbin, 
+		visualize_root + 'raw_samples-histogram.png', 'Raw driving data')
+	
+	# reduce occurence of low-angle data
+	filtered_low = filter_low_steering_angles(data)
+
+	# see what data distribution look now
+	plot_histogram(filtered_low, nbin, 
+		visualize_root + 'filtered_samples-histogram.png', 'Low-steering angles removed')
+
+	data_with_lf = augment_with_lf_camera(filtered_low)
+
+	plot_histogram(data_with_lf, nbin, 
+		visualize_root + 'with_lf_samples-histogram.png', 'With L/F angles')
+
+	# brightness
+	augmented_brightness = augment(change_brightness, data_with_lf, 'augment_brightness_')
+
+	current_data.extend(data_with_lf)
+	current_data.extend(augmented_brightness)
+
+	plot_histogram(data_with_lf, nbin, 
+		visualize_root + 'with_brightness_samples-histogram.png', 'After adding brightness')
+
+	# shadow
+	data_with_shadow = augment(add_shadow, data_with_lf, 'augment_shadow_')
+	current_data.extend(data_with_shadow)
+
+	plot_histogram(current_data, nbin, 
+		visualize_root + 'with_shadow_samples-histogram.png', 'After adding shadow')
+
+	#shift
+	data_with_shift = augment(shift_image, data_with_lf, 'augment_shift_')
+	current_data.extend(data_with_shift)
+
+	plot_histogram(current_data, nbin, 
+		visualize_root + 'with_shift_samples-histogram.png', 'After adding shift')
+
+	# flipping
+	data_flipped = augment(flip_image, data_with_lf, 'augment_flipped_')
+	current_data.extend(data_flipped)
+
+	plot_histogram(current_data, nbin, 
+		visualize_root + 'with_flipped_samples-histogram.png', 'After flipping')
+
+	return current_data
+
+def main():
+	samples = get_driving_data(read_driving_log(DRIVING_LOG))
+	augment_and_visualize_data(samples)
+
+if __name__ == "__main__":
+    main()
